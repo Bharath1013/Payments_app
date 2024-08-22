bb9fc3bcdb1b9e57f625c1727e6154720c4b9572
Bharath1013 <139207506+Bharath1013@users.noreply.github.com>
2024-08-22 10:09:56 +0530

    added history route

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,7 +1,7 @@
 // backend/routes/account.js
 const express = require('express');
 const { authMiddleware } = require('../middleware');
-const { Account } = require('../db');
+const { Account, User } = require('../db');
 const { default: mongoose } = require('mongoose');
 
 const router = express.Router();
@@ -17,6 +17,7 @@ router.get("/balance", authMiddleware, async (req, res) => {
     })
 });
 
+
 router.post("/transfer", authMiddleware, async (req, res) => {
     const session = await mongoose.startSession();
 
@@ -25,7 +26,10 @@ router.post("/transfer", authMiddleware, async (req, res) => {
 
     // Fetch the accounts within the transaction
     const account = await Account.findOne({ userId: req.userId }).session(session);
-
+    const fromuser = await User.findOne({ _id: req.userId }).session(session);
+   const fromusername = fromuser.firstName+''+fromuser.lastName
+   const touser = await User.findOne({ _id: to }).session(session);
+   const tousername= touser.firstName+' '+touser.lastName
     if (!account || account.balance < amount) {
         await session.abortTransaction();
         return res.status(400).json({
@@ -34,7 +38,6 @@ router.post("/transfer", authMiddleware, async (req, res) => {
     }
 
     const toAccount = await Account.findOne({ userId: to }).session(session);
-
     if (!toAccount) {
         await session.abortTransaction();
         return res.status(400).json({
@@ -43,15 +46,58 @@ router.post("/transfer", authMiddleware, async (req, res) => {
     }
 
     // Perform the transfer
-    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
-    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
-
+    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } , $push: {
+        transactions: {
+          userId: to,
+          username:tousername,
+          transaction: -amount, // Negative to indicate debit
+          date: new Date()
+        }
+      }}).session(session);
+    await Account.updateOne({ userId: to }, {  $inc: { balance: amount } , $push: {
+        transactions: {
+          userId: req.userId,
+          username:fromusername,
+          transaction: amount, // Negative to indicate debit
+          date: new Date()
+        }
+      }}).session(session);
+    
     // Commit the transaction
     await session.commitTransaction();
     res.json({
         message: "Transfer successful"
     });
 });
+
+
+router.get('/transactions',authMiddleware, async (req, res) => {
+  const userId = req.userId; // Assume userId is passed as a query parameter
+
+  try {
+    // Find the account by userId
+    const account = await Account.findOne({ userId: userId })
+
+    if (!account) {
+      return res.status(404).json({ message: 'Account not found' });
+    }
+
+    // Map transactions to the desired structure
+    const formattedTransactions = account.transactions.map(transaction => ({
+      username: transaction.username,
+      date: transaction.date,
+      amount: transaction.transaction
+    }));
+
+    res.json(formattedTransactions);
+  } catch (error) {
+    console.error('Error fetching transactions:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+module.exports = router;
+
 // router.post("/transfer", authMiddleware, async (req, res) => {
 //     const { amount, to } = req.body;
 
@@ -95,4 +141,5 @@ router.post("/transfer", authMiddleware, async (req, res) => {
 //         message: "Transfer successful"
 //     })
 // });
+
 module.exports = router;
